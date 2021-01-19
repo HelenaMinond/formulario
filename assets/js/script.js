e2ed6569c59d37dca032d9f5cd0ebb3e85871d6d
Helena Santana <helena.minond@gmail.com>
2021-01-19 11:12:49 -0300

    desarrollando codigo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-var obtenerDatos = function () {
+//Falta validar:
+//-Formato de email
+//-Que el telefono sea un numero, aunque el type:number no deja ingresar letras ni saltarse espacios.
 
-    var nombre = document.getElementById("nombre").value;
-    var apellidos = document.getElementById("apellidos").value;
-    var correo = document.getElementById("correo").value;
-    var usuario = document.getElementById("usuario").value;
-    var contraseña = document.getElementById("contraseña").value;
-    var telefono = document.getElementById("telefono").value;
+let boton = document.getElementById("boton");
 
-    alert ("¡Bienvenido!\nSus datos de registros son: " + "\nNombre: " + nombre + " " + apellidos + "\nUsuario: " + usuario + "\nContraseña: "+ contraseña);
+boton.addEventListener("click", function() {
 
-}
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let correo = document.getElementById("correo").value;
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let telefono = document.getElementById("telefono").value;
+
+    if (nombre === null || apellidos === null || correo === null || usuario === null || contraseña === null || telefono === null) {
+        alert ("Ingrese todos los datos solicitados para registrarse");
+    }
+
+    else if (telefono.toString().length <= 15 || nombre.trim().length <= 30 || apellidos.trim().length <= 80 || correo.trim().length <= 100 || usuario.trim().length <= 20) {
+        alert ("¡Bienvenido!\nSus datos de registros son: " + "\nNombre: " + nombre + " " + apellidos + "\nUsuario: " + usuario + "\nContraseña: "+ contraseña);
+    }
+})
+
+    
